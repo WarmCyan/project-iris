c36e4b72d73f7655322ae4d421aff3d05ccb0469
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-08-06 11:22:13 -0400

    Switched from parsing with regular expressions to manual parsing

--- firsttests/being.py
+++ firsttests/being.py
@@ -1,14 +1,18 @@
 class Being:
     Concepts = {
             "exist":"[mutate (exist) [concept-list]][query \"approve\"][remember (exist) [recall \"approval\"]][exist]",
-            "mutate":"[python \"print(self.cache)\"]",
+            #"mutate":["[python \"print(self.cache[self.level][0])\"]",2], # 2 represents number of arguments it takes
+            "mutate":"[python \"print(self.cache[self.level][0])\"]", # 2 represents number of arguments it takes
             "query":"",
             "remember":"",
             "recall":"",
-            "concept-list":"[python \"self.cache = self.entity.Concepts\"]"
+            "concept-list":"[python \"self.cache[self.level][self.argNum[self.level]] = self.entity.Concepts\"]"
+            #"approval":"[python \"self.cache[self.level] # YES, do this
             }
 
-    Memory = {}
+    Memory = {
+            "approval":0
+            }
 
 
     #def mutate(concept, conceptList):
--- firsttests/life.py
+++ firsttests/life.py
@@ -5,11 +5,13 @@ class Interpreter:
 
     entity = None
     conceptPattern = """\[([\w\-\_]+)((?:(?:\s?\[\w+[\[()\s\w\-\_\."]*\])|(?:[\s"()\w\-\_\$\.\=]*))*)\]"""
-    argumentPattern = """((?:"[\w\s\-\_\.(?:\\\")\=$]*")|(?:\([\w\-\_\.]+\))|(?:\[(?:[\w\-\_\.]+)(?:(?:(?:\s?\[\w+[\[()\s\w\-\_\."]*\])|(?:[\s"()\w\-\_\.\$\=]*))*)\]))"""
+    argumentPattern = """((?:"[\w\s\-\_\.(?:\\\")\=$\[\]]*")|(?:\([\w\-\_\.]+\))|(?:\[(?:[\w\-\_\.]+)(?:(?:(?:\s?\[\w+[\[()\s\w\-\_\."]*\])|(?:[\s"()\w\-\_\.\$\=]*))*)\]))"""
     compiledConceptPattern = None
     compiledArgumentPattern = None
 
-    
+    level = 0
+    cache = [[],[],[],[],[],[],[]] # find better way to do this!!!
+    argNum = [0,0,0,0,0,0,0,0] # AND THIS!!!
     
     def __init__(self):
         self.compiledConceptPattern = re.compile(self.conceptPattern)
@@ -24,12 +26,89 @@ class Interpreter:
         #print(thing)
         #print(self.entity.Concepts["exist"])
         #self.RunConcept(self.entity.Concepts["exist"])
-        self.RunConcept("[mutate [concept-list]]")
+
+
+
+        #self.RunConcept("[mutate [concept-list]]")
+        self.ParseConcepts("[mutate [concept-list]][thing][thingy (and) \"yes\"]")
+
+
+
         #self.RunConcept("[python \"eval(\"self.entity.Concepts\")\"]")
+
+    #def ParseHighConcepts(self, concept):
+        #matches = re.findall(self.compiledConceptPattern, concept)
+        #return matches
+
+
+    def ParseConcepts(self, concept):
+        braceLevel = 0
+        quoteLevel = 0
+        parenLevel = 0
+        recordingConcept = True
+        conceptNum = 0
+        argNum = -1
+        concepts = []
+
+        ESCAPE = False
+
+        for character in concept:
+            if character == "[": braceLevel += 1
+            elif character == "]":
+                braceLevel -= 1
+                if braceLevel == 0:
+                    conceptNum += 1
+                    recordingConcept = True # if completely finished that part of concept, and potentially starting new one, turn recording back on
+                    argNum = -1
+            elif character == "(": parenLevel += 1
+            elif character == ")": parenLevel -= 1
+            elif character == "\\": 
+                ESCAPE = True 
+                continue
+            elif character == "\"" and not ESCAPE:
+                if quoteLevel == 0: quoteLevel += 1
+                elif quoteLevel == 1: quoteLevel -= 1
+            elif character == " ": recordingConcept = False
+            elif recordingConcept:
+                try: concepts[conceptNum]
+                except IndexError: concepts.append([])
+                try: concepts[conceptNum][0]
+                except IndexError:
+                    concepts[conceptNum].append("") # concept name
+                    concepts[conceptNum].append([]) # args
+                concepts[conceptNum][0] += character
+                
+            # argument handling
+            if not recordingConcept:
+                if character == " " and braceLevel == 1 and quoteLevel == 0: # space signifies next argument (assuming not in higher level of braces
+                    concepts[conceptNum][1].append("")
+                    argNum += 1
+                else:
+                    concepts[conceptNum][1][argNum] += character
+                
+            if ESCAPE: ESCAPE = False
+                
+
+        print(concepts)
+
+        
+
+    def ParseAllConcepts(self, concept): #this won't get literally every concept, only top two levels
+        matches = self.ParseHighConcepts(concept)
+        allConcepts = matches
+        for match in matches:
+            args = match[1][1:]
+            argmatches = re.findall(self.compiledArgumentPattern, args)
+            for arg in argmatches:
+                if arg.startswith("[") or arg.startswith("("):
+                    allConcepts.append(arg)
+
+        return allConcepts
+            
 
     def RunConcept(self, concept):
         print(concept)
-        matches = re.findall(self.compiledConceptPattern, concept)
+        matches = self.ParseHighConcepts(concept)
         #print(matches)
         for match in matches:
             print(match[0])
@@ -38,10 +117,14 @@ class Interpreter:
             args = match[1][1:]
             argmatches = re.findall(self.compiledArgumentPattern, args)
 
+            self.argNum[self.level] = 0
             for arg in argmatches:
                 print("ARGUMENT - " + str(arg))
                 if arg.startswith("["):
+                    self.level += 1
                     self.RunConcept(arg)
+                    self.level -= 1
+                self.argNum[self.level] += 1
             
 
             # run python if it's python
@@ -52,5 +135,5 @@ class Interpreter:
                 #print(code)
                 exec(code)
             else:
-                runstring = self.entity.Concepts[match[0]]
+                runstring = self.entity.Concepts[match[0]] #[0]
                 self.RunConcept(runstring)
